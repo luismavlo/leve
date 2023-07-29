4c612d91c4983eca0d3fbbb6bdca1b4847d6cbca
Luis Miguel <lmavendano1597@gmail.com>
2023-07-29 09:24:00 -0500

    fixing errors

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,148 +1,47 @@
-const Repair = require('./../models/models.repairs');
+const { Router } = require('express');
+const {
+  updateUser,
+  deleteUser,
+  findUsers,
+  findUser,
+  updatePassword,
+} = require('../controllers/users.controller');
+const {
+  protect,
+  protectAccountOwner,
+} = require('../middlewares/auth.middleware');
+const { validIfExistUser } = require('../middlewares/user.middleware');
+const {
+  updateUserValidation,
+  updatePasswordUserValidation,
+} = require('../middlewares/validations.middleware');
 
-exports.findAllRepair = async (req, res) => {
-  try {
-    const repairs = await Repair.findAll({ where: { status: 'pending' } });
+const router = Router();
 
-    return res.status(200).json({
-      status: 'success',
-      repairs,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      status: 'fail',
-      message: 'Internal server error',
-      error,
-    });
-  }
-};
+router.get('/', findUsers);
 
-exports.createRepair = async (req, res) => {
-  try {
-    const { date, usersId } = req.body;
+router.get('/:id', validIfExistUser, findUser);
 
-    const repair = await Repair.create({
-      date,
-      usersId,
-    });
+router.use(protect);
 
-    return res.status(201).json({
-      status: 'success',
-      message: 'Repair created',
-      repair,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      status: 'fail',
-      message: 'Internal server error',
-      error,
-    });
-  }
-};
+router.patch(
+  '/:id',
+  updateUserValidation,
+  validIfExistUser,
+  protectAccountOwner,
+  updateUser
+);
 
-exports.findOneRepair = async (req, res) => {
-  try {
-    const { id } = req.params;
+router.patch(
+  '/password/:id',
+  updatePasswordUserValidation,
+  validIfExistUser,
+  protectAccountOwner,
+  updatePassword
+);
 
-    const repair = await Repair.findOne({
-      where: {
-        id,
-        status: 'pending',
-      },
-    });
+router.delete('/:id', validIfExistUser, protectAccountOwner, deleteUser);
 
-    if (!repair) {
-      return res.status(404).json({
-        status: 'error',
-        message: 'Repair not found',
-      });
-    }
-
-    return res.status(200).json({
-      status: 'success',
-      message: 'Repair found',
-      repair,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      status: 'fail',
-      message: 'Internal server error',
-      error,
-    });
-  }
-};
-
-exports.updateRepair = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { status } = req.body;
-
-    const repair = await Repair.findOne({
-      where: {
-        id,
-        status: 'pending',
-      },
-    });
-
-    if (!repair) {
-      return res.status(404).json({
-        status: 'error',
-        message: 'Repair not found',
-      });
-    }
-
-    await repair.update({ status: 'completed' });
-
-    return res.status(200).json({
-      status: 'success',
-      message: 'Repair updated',
-      repair,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      status: 'fail',
-      message: 'Internal server error',
-      error,
-    });
-  }
-};
-
-exports.deleteRepair = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { status } = req.body;
-
-    const repair = await Repair.findOne({
-      where: {
-        id,
-        status: 'pending',
-      },
-    });
-
-    if (!repair) {
-      return res.status(404).json({
-        status: 'error',
-        message: 'Repair not found',
-      });
-    }
-
-    await repair.update({ status: 'cancelled' });
-
-    return res.status(200).json({
-      status: 'success',
-      message: 'Repair cancelled',
-      repair,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      status: 'fail',
-      message: 'Internal server error',
-      error,
-    });
-  }
+module.exports = {
+  usersRouter: router,
 };
